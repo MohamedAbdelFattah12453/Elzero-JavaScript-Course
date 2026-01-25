d40a115183097fd05d1e6cd37eec00fa2032f26d
Mohamed <mido2011fattah@gmail.com>
2026-01-25 21:26:21 +0200

    feat: add lesson 06 code and exercises

---
+++ main.js
@@ -0,0 +1,29 @@
+//wait The Window To Load
+/*
+ * Project: Elzero Web School - JavaScript Assignments
+ * Lesson: [6: Lessons 1-9]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-01-25
+ */
+window.onload = function () {
+    document.querySelector("h1").style.color = "blue";
+};
+
+// Single - line - Comment
+// Single - line - Comment
+// Single - line - Comment
+// ctrl + /
+
+
+
+
+
+
+
+
+
+
+
+
+
