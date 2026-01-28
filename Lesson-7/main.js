78f46732f6a69bb1fd29aad45489674cf953be3c
Mohamed <mido2011fattah@gmail.com>
2026-01-28 04:11:49 +0200

    peat:Add to Lesson 8 and exerices

---
+++ Lesson-7/main.js
@@ -0,0 +1,18 @@
+/*
+ * Project: Elzero Web School - JavaScript Assignments
+ * Lesson: [7: Lessons 1-9]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-01-27
+ */
+/*
+--Output To Screen
+---window.alert()
+---document.write()
+---console.log()
+
+--Syntax
+*/
+// window.alert("Hello From JS File");
+document.write("<h1>hello<span>Page</span></h1>");
+console.log("Hello From JS File");
---
+++ Lesson-8/main.js
@@ -0,0 +1,33 @@
+/*
+ * Project: Elzero Web School - JavaScript Assignments
+ * Lesson: [8: Lessons 1-9]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-01-27
+ */
+/*
+--Console Methods
+--Log
+--error
+---table
+
+-Web ApI
+
+--Styling Console
+--Directive-%c
+*/
+console.log("log");
+console.error("error");
+
+console.table(["Osama", "Ahmed", "Adam"]);
+console.log("Hello From %cJS%cFile", "color: red; font-size: 40px", "color: blue; font-size: 40px")
+
+
+
+
+
+
+
+
+
+
