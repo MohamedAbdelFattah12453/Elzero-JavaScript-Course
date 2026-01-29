96a4e5346d14f30343ad6f7181fc988f658c574f
Mohamed <mido2011fattah@gmail.com>
2026-01-29 02:41:26 +0200

    peat : Add to Lesson 9 and execrices

---
+++ Lesson-9/main.js
@@ -0,0 +1,22 @@
+/*
+ * Project: Elzero Web School - JavaScript Assignments
+ * Lesson: [9: Lessons 1-9]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-01-29
+ */
+/*
+..ES6
+*/ 
+
+var myName = "Mohamed";
+
+console.log("Hello" + myName);
+console.log(`Hello ${myName}` )
+
+
+
+
+
+
+
