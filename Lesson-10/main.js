f6cfc76ba5fdf94d3b9072b49a6198d4c8d8599e
Mohamed <mido2011fattah@gmail.com>
2026-02-01 21:13:35 +0200

    Add: Data Types introduction and typeof operator

---
+++ Lesson-10/main.js
@@ -0,0 +1,33 @@
+/*
+ * Project: Elzero Web School - JavaScript Assignments
+ * Lesson: [10: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-1
+ */
+/*
+    Data Types Intro
+    String
+    Number
+    Array => Object
+    Object
+    Boolean
+*/
+
+console.log("Moahmed Abdel Fattah");
+console.log(typeof ("Moahmed Abdel Fattah"));
+console.log(typeof 5000);
+console.log(typeof 5000.99);
+console.log(typeof [10, 15, 17]);
+console.log(typeof ["Os", "Mh", "Ad"]);
+console.log(typeof { name: "Mohamed", age: 15, country: "Eg" });
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
+
+
