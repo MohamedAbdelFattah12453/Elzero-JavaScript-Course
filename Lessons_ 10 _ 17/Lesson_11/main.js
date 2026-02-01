54b6b495dc3d6aa1c72fda70a6cadd3b6c25bd6c
Mohamed <mido2011fattah@gmail.com>
2026-02-01 23:02:04 +0200

    Practicing Variables (var, let, const) and naming rules

---
+++ Lessons_ 10 _ 17/Lesson_11/main.js
@@ -0,0 +1,31 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [11: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-1
+ */
+
+/*
+    Variables Intro
+    What is Variables ?
+    Why We Use variables ?
+    Declare A Variable And Use
+    Syntax ( Keyword | Variable Name | Assignment Operator | Variable Value)
+    Varisble Without Var
+    Multiple Varianles in The Same Line
+    Id And Global Variable
+    Loosely Typed vs Strongly Typed
+*/
+var user = "MOHAMED";
+console.log(user);
+
+
+
+
+
+
+
+
+
+
