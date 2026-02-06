bb541c758a2e66314982f621b98204de03ebb989
Mohamed <mido2011fattah@gmail.com>
2026-02-06 21:52:43 +0200

    Understanding String Syntax and Escaping Characters

---
+++ Lessons_ 10 _ 17/Lesson_13/main.js
@@ -0,0 +1,31 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [13: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-6
+ */
+/*
+
+    Var
+        Redeclare (Yes)
+        Access Before Declare (Undefined)
+        Variable Scope Drama [Added To Window] ()
+        Block or Function Scope
+    
+    let 
+        Redeclare (No => Error)
+        Access Before Declare (Error)
+        Variable Scope Drama ()
+        Block or Function Scope
+    
+    const
+        Redeclare (No => Error)
+        Access Before Declare (Error)
+        Variable Scope Drama ()
+        Block or Function Scope
+*/
+
+
+
+
