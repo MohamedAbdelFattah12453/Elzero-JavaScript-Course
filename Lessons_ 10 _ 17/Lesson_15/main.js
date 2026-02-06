90e5ac53a8216db711a68ee00ec7b38988905769
Mohamed <mido2011fattah@gmail.com>
2026-02-06 23:50:43 +0200

    feat: solve variables and concatenation challenge - Part 1

---
+++ Lessons_ 10 _ 17/Lesson_15/main.js
@@ -0,0 +1,19 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [15: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-6
+ */
+
+/*
+    Concatenation
+*/
+let a = "we Love";
+let b = "JavaScript";   
+
+document.write(a  + " " + b);
+
+console.log(a, b)
+
+
