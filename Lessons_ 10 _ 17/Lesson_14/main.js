c64d3e0d2904dcff8f7fd7775c1fbd451491304e
Mohamed <mido2011fattah@gmail.com>
2026-02-06 22:39:01 +0200

    feat: practice string syntax and escape characters

---
+++ Lessons_ 10 _ 17/Lesson_14/main.js
@@ -0,0 +1,20 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [14: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-6
+ */
+/*
+    String syntax + Character Escape Sequences
+    / Escape + Line Continue
+    /n
+*/ 
+console.log('Elzero web "School"');
+console.log("Elzero web 'School'");
+console.log("Elzero web \"School\"");
+console.log('Elzero \\ web \'School\'');
+console.log("Elzero \
+web \
+School")
+console.log("Elzero \nWeb \nSchool")
