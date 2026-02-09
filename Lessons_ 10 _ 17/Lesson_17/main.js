dfb04527b699791db9da0545002cab14f608dff8
Mohamed <mido2011fattah@gmail.com>
2026-02-09 05:13:52 +0200

    feat: solve JavaScript template literals and variables challenge (Lesson 17)

---
+++ Lessons_ 10 _ 17/Lesson_17/main.js
@@ -0,0 +1,23 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [17: Lessons 10-17]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-9
+ */
+let title = "Elzero", Description = "Elzero Web School",Data = "25/10"
+
+let markUp = `
+<div>
+<h3>Hello ${title}</h3>
+<p>${Description}</P>
+<span>${Data}</span>
+</div>
+`;
+document.write(markUp.repeat(4));
+
+
+
+
+
+
