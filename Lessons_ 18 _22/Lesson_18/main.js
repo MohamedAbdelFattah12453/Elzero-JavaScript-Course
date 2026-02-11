095918e769a01e7a933736c2055f108792fcc452
Mohamed <mido2011fattah@gmail.com>
2026-02-12 01:52:41 +0200

    feat: practice basic math operators (+, -, *, /, %

---
+++ Lessons_ 18 _22/Lesson_18/main.js
@@ -0,0 +1,31 @@
+/*
+    Arithmetic Operators
+    + Addition
+    - Subtraction
+    * Multiplication
+    / Division
+    ** Exponenetiation (ES7)
+    % Modulus (Division Remainder)
+    ++ Increment [Post / Pre]
+    -- Derement [Post / Pre]
+*/
+console.log(10 + 20);
+console.log(10 + "Mohamed")
+
+console.log(10 - 20);
+console.log(10 - "Mohamed") // NaN
+console.log(typeof  NaN)
+
+console.log(10 * 20);
+console.log(10 * -20)
+
+console.log(20 / 5);
+console.log(20 / 3);
+
+console.log(2 ** 4)
+console.log(2 * 2 * 2 * 2)
+
+console.log(10 % 2)
+console.log(11 % 2) // Remove 1
+
+console.log(34 / 2) // Remove 1
