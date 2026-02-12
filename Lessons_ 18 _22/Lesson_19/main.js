68b27a64b1e1706ab57e7326e9e3946e213f421a
Mohamed <mido2011fattah@gmail.com>
2026-02-12 02:25:13 +0200

    feat: understand unary plus and minus effects on data types

---
+++ Lessons_ 18 _22/Lesson_19/main.js
@@ -0,0 +1,43 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [19: Lessons 18-22]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-11
+ */
+/*
+    + Unary Plus [ Return Number If Its Not Number]
+    - Unary Negation [ Return Number If Its Not Number + Negates It]
+    Tests
+    Normal Number
+    String Number
+    String Negative Number
+    String Text
+    Float
+    Hexadecimal Numeral System => 0xFF
+    null
+    false
+    true
+*/
+console.log(+100);
+console.log(+"100");
+console.log(+"-100");
+console.log(+"Mohamed");
+console.log(+"15.5");
+console.log(+"0xFF");
+console.log(+null);
+console.log(+false);
+console.log(+true);
+
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(-"Mohamed");
+console.log(-"15.5");
+console.log(-"0xFF");
+console.log(-null);
+console.log(-false);
+console.log(-true);
+
+console.log(Number("100"));
+
