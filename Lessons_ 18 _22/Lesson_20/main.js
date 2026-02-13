29477473aea5921216bde495f004d49a68344525
Mohamed <mido2011fattah@gmail.com>
2026-02-13 03:41:12 +0200

    eat: practice compound assignment operators (+=, -=, *=)

---
+++ Lessons_ 18 _22/Lesson_20/main.js
@@ -0,0 +1,24 @@
+/*
+ * Project: Elzero Web School - JavaScript Lessons
+ * Lesson: [19: Lessons 18-22]
+ * Description:"Understanding JavaScript data types and how to check them using the typeof operator."
+ * Author: Mohamed Abdel Fattah
+ * Date: 2026-02-11
+ */
+
+/*
+Type Coercion (Type Casting)
+    +
+    -
+    ""  -  2
+    false   true
+*/
+
+let a = "10";
+let b = 20;
+let c = true;
+
+console.log(+a + b + c)
+
+
+
