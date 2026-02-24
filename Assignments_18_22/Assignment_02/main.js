a3fc6ddedcce46e4cecda3b62eede7b0baac838b
Mohamed <mido2011fattah@gmail.com>
2026-02-24 03:51:48 +0200

    feat(ops): solve assignment 02 multiple ways to output 6

---
+++ Assignments_18_22/Assignment_02/main.js
@@ -0,0 +1,21 @@
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num + true + true + true); // 6
+
+// Solution Three
+console.log(num * (true + true)); // 6
+
+// Solution Four
+console.log(num << true); // 6
+
+// Solution Five
+console.log((num * num) - num); // 6
+
+// Solution Six
+console.log(num ** (true + true) - num); // 6
+
+
