c6477a5ce18f3d3c0c529dd1de9e1fbc0c618cc3
Mohamed <mido2011fattah@gmail.com>
2026-03-09 02:20:36 +0200

    Solve JavaScript string check task

---
+++ Assignments _ 26 _ 30/Assignments_ 2/main.js
@@ -0,0 +1,8 @@
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(letterZ.includes(letterZ)); // True
+console.log(letterE.toLowerCase().startsWith(letterE)); // True
+console.log(letterO.toLowerCase().endsWith(letterO.toLowerCase())); // True
