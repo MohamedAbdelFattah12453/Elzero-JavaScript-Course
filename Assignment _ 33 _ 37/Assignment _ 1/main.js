4929311f6412e9fc072d0c3ac2295a21c58297e9
Mohamed <mido2011fattah@gmail.com>
2026-03-15 03:05:05 +0200

    feat(logic): solve assignment 01 - compare number value and type

---
+++ Assignment _ 33 _ 37/Assignment _ 1/main.js
@@ -0,0 +1,16 @@
+// Test Case 1
+let num1 = 9; // "009"
+
+// Test Case 2
+let num2 = 20; // "020"
+
+// Test Case 3
+let num3 = 110; // "110"
+
+if (num1 < 10) {
+    console.log(`00${num1}`)
+} if (num2 > 10 && num2 < 100) {
+    console.log(`0${num2}`)
+} if (num3 >= 100) {
+    console.log(`${num3}`)
+}
