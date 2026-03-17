47fb3f058c6dd098f501df63a09f8e6f4c0ecaef
Mohamed <mido2011fattah@gmail.com>
2026-03-17 10:58:59 +0200

    feat: solve lesson 41 - accessing and modifying array elements

---
+++ Lesson _ 40 _ 47/Lesson _ 41/main.js
@@ -0,0 +1,14 @@
+/*
+    Arrays Methods
+    Length
+*/
+
+// Index Start From 0 [0, 1, 2, 3, 4]
+
+let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa", "Osama"];
+
+myFriends.length = 2;
+
+console.log(myFriends);
+
+
