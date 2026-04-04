bf84ca4eb2ea2a6f765bb0d9cb781ebc99cdb8ad
Mohamed <mido2011fattah@gmail.com>
2026-04-04 12:48:30 +0200

    Lesson 50: Implementation of Nested Loops with Practical Training

---
+++ Lessons _ 48 _56/Lesson _ 50/main.js
@@ -0,0 +1,24 @@
+/*
+    Loop
+    Nested Loops
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
+
+let color = ["Red", "Green", "Black"];
+
+let models = [2020, 2021];
+
+for (let i = 0; i < products.length; i++) {
+    console.log("#".repeat(15));
+    console.log(`# ${products[i]}`);
+    console.log("#".repeat(15));
+    console.log("Colors :")
+    for (let j = 0; j < color.length; j++) {
+        console.log(`- ${color[j]}`);
+    }
+    console.log("Models :");
+    for (let k = 0; k < models.length; k++) {
+        console.log(`- ${models[k]}`);
+    }
+}
