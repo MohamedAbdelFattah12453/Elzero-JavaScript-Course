fafb06d07a6b05836a0d6a2eb5a261c0eea11b6c
Mohamed <mido2011fattah@gmail.com>
2026-04-11 15:53:44 +0200

    Lesson 55: Compare While vs Do-While Loop Structures

---
+++ Lessons _ 48 _56/Lesson _ 55/main.js
@@ -0,0 +1,15 @@
+/*
+    Loop
+    Do / While
+*/
+
+let product = ["Keyword", "Mouse", "Pen", "Pad", "Monitor", "iphone"];
+
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while (false)
+
+console.log(i);
