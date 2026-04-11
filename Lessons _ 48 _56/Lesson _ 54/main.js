368657a567aab750c1bfe5aecf95e8cf4d15e98e
Mohamed <mido2011fattah@gmail.com>
2026-04-11 15:45:28 +0200

    Lesson 54: Introduction to While Loop and Condition Logic

---
+++ Lessons _ 48 _56/Lesson _ 54/main.js
@@ -0,0 +1,13 @@
+/*
+    Loop
+    While
+*/
+
+let products = ["Keyword", "Mouse", "Pen", "Pad", "Monitor", "iphone"];
+
+let index = 0;
+
+while (index < products.length) {
+    console.log(products[index]);
+    index += 1;
+}
