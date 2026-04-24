c5ea794150585ab2c538c1c44b62445916e023f2
Mohamed <mido2011fattah@gmail.com>
2026-04-24 19:22:35 +0300

    Lesson 57: Function Introduction and Basic Syntax

---
+++ Lessons _ 57 _ 63/Lesson _ 57/main.js
@@ -0,0 +1,17 @@
+/*
+    Function
+    What Is Function ?
+    User Defined vs Bulit In
+    Syntax + Basic Usage
+    Example From Real Life
+    Parameter + Argument
+    Practical Example
+*/
+
+function sayHello(userName) {
+    console.log(`Hi ${userName}`);
+}
+
+sayHello("Osama");
+sayHello("Sayed");
+sayHello("Ali");
