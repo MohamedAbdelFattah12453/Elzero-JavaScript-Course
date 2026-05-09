981a28eb2f4789aee176f3161920f7e389b003cd
Mohamed <mido2011fattah@gmail.com>
2026-05-09 03:22:53 +0300

    Lesson 65: Arrow Function Syntax and Shortened Forms

---
+++ Lessons _ 64 _ 70/Lesson _ 65/main.js
@@ -0,0 +1,52 @@
+/*
+    Function
+    Function Inside Function
+    Return Function
+*/
+// Example 1
+
+// function sayMessage(fName, lName) {
+// let message = `Hello`;
+// Nested Function
+// function concatMsg() {
+//  message = `${message} ${fName} ${lName}`;
+//  }
+//  concatMsg();
+//  return message;
+// }
+
+// console.log(sayMessage("Mohamed", "Abdel Fattah"));
+
+// Example 2
+
+// function sayMessage(fName, lName) {
+//     let message = `Hello`;
+//     // Nested Function
+//     function concatMsg() {
+//         return `${message} ${fName} ${lName}`;
+//     }
+//     return concatMsg();
+// }
+
+// console.log(sayMessage("Mohamed", "Abdel Fattah"));
+
+// Example 3
+
+function sayMessage(fName, lName) {
+    let message = `Hello`;
+    // Nested Function
+    function concatMsg() {
+
+        function getFullName() {
+
+            return `${fName} ${lName}`;
+
+        }
+
+        return `${message} ${getFullName()}`;
+    }
+
+    return concatMsg();
+}
+
+console.log(sayMessage("Mohamed", "Abdel Fattah"));
