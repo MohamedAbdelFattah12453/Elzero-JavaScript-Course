2250fcdf3cfe93086c7485422410d8f702e7edc4
Mohamed <mido2011fattah@gmail.com>
2026-05-10 12:37:53 +0300

    Lesson 68: Block Scope and Lexical (Static) Scope Analysis

---
+++ Lessons _ 64 _ 70/Lesson _ 68/main.js
@@ -0,0 +1,28 @@
+/*
+    Scope
+    Block Scope [If, Switch, For]
+*/
+
+var x = 10;
+
+if (10 === 10) {
+    let x = 50;
+    console.log(`From If Block ${x}`);
+}
+
+console.log(`From Global ${x}`);
+
+// var a = 1;
+// let b = 2;
+
+// function showText() {
+//     var a = 1;
+//     let b = 2;
+//     console.log(`Function - From Local ${a}`);
+//     console.log(`Function - From Local ${b}`);  
+// }
+
+// console.log(`From Global ${a}`);
+// console.log(`From Global ${b}`);
+
+// showText();
