bc200903a0777cd5b113f19b41c76a37e7133632
Mohamed <mido2011fattah@gmail.com>
2026-05-30 03:44:04 +0300

    Assignment 04: Implement specialMix Function with Dynamic Parsing and Type Checks

---
+++ Assignment _ 64 _ 70/Assignment _ 4/main.js
@@ -0,0 +1,24 @@
+function specialMix(...data) {
+    let total = 0;
+    let foundNumbers = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (typeof data[i] === "number") {
+            total += data[i]
+            foundNumbers++;
+        } else if (!isNaN(parseInt(data[i]))) {
+            total += parseInt(data[i]);
+            foundNumbers++;
+        }
+        
+    }
+    if (foundNumbers > 0) {
+        return total;
+    } else {
+        return "All Is Strings";
+    }
+}
+
+console.log(specialMix(10, 20, 30)); // 60
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
