33e080ec54b45bf3ee5cd0bcb3b509ca20110bc7
Mohamed <mido2011fattah@gmail.com>
2026-06-03 01:48:07 +0300

    Lesson 76 Reduce Practice: Ultimate Use Cases

---
+++ Lessons _ 71 _ 78/Lesson _ 76/main.js
@@ -0,0 +1,27 @@
+/*
+    Reduce
+    -- Longest Word
+    -- Remove Charcters + Use Reduce
+*/
+
+let theBiggest = ["Bla", "Propaganda", "Other", "AAA", "Battery", "Test", "Propaganda_Two" ];
+
+let check = theBiggest.reduce(function (acc, current, index, arr) {
+    console.log(`Acc => ${acc}`);
+    console.log(`Current Element => ${current}`);
+    console.log(acc.length > current.length ? acc : current);
+    console.log(`#############`)
+    return acc.length > current.length ? acc : current;
+});
+
+console.log(check);
+
+let removeChars = ["E", "@", "@", "L", "Z", "@", "@", "E", "R", "@", "O"];
+
+let finalString = removeChars.filter(function (ele) {
+    return !ele.startsWith("@");
+}).reduce(function (acc, current) {
+    return `${acc}${current}`;
+});
+
+console.log(finalString);
