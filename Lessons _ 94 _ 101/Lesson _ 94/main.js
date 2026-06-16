88c7f1b20def32abf1f838ea635566bff31670bb
Mohamed <mido2011fattah@gmail.com>
2026-06-16 13:40:10 +0300

    Lesson 94: Event Simulation - Click Focus Blur

---
+++ Lessons _ 94 _ 101/Lesson _ 94/main.js
@@ -0,0 +1,18 @@
+/*
+    DOM [Events Simulation]
+    - click
+    - focus
+    - blur
+*/
+
+let two = document.querySelector(".two");
+let one = document.querySelector(".one");
+
+// window.onload = function () {
+//     two.focus();
+// };
+
+// one.onblur = function () {
+//     document.links[0].click();
+// };
+
