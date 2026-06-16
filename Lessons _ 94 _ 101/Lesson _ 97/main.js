955cd8ea3920d0a84ac56efe587c9ecd66ab0d5b
Mohamed <mido2011fattah@gmail.com>
2026-06-16 14:56:45 +0300

    Lesson 97: Deal With Elements - Before After Prepend Append Remove

---
+++ Lessons _ 94 _ 101/Lesson _ 97/main.js
@@ -0,0 +1,13 @@
+/*
+    DOM [Deal With Elements]
+    before [Elements || String]
+    after [Elements || String]
+    append [Elements || String]
+    prepend [Elements || String]
+    remove
+*/
+
+let elemnet = document.getElementById("my-div");
+let createdP = document.createElement("p");
+
+// elemnet.remove(); 
