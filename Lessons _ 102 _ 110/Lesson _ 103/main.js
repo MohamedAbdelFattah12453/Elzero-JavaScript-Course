8a4c028c08730c84af7827972db1033479ea1378
Mohamed <mido2011fattah@gmail.com>
2026-06-22 22:46:40 +0300

    Lessons 102-110: Video 103 - BOM [Alert, Confirm, Prompt]

---
+++ Lessons _ 102 _ 110/Lesson _ 103/main.js
@@ -0,0 +1,24 @@
+/*
+    BOM [Browser Object Model]
+    - alert(Message) => Need No Response Only OK Available
+    - confirm(Message) => Need Response And Return A Boolean
+    - prompt(Message, Default Message) => Collect Data
+*/
+
+// alert("Test");
+// console.log("Test");
+
+// let confirmMsg = confirm("Are You Sure?");
+
+// console.log(confirmMsg);
+
+// if (confirmMsg === true) {
+//     console.log("Item Deleted");
+// } else {
+//     console.log("Item Not Deleted");
+// }
+
+let PromptMsg = prompt("Good Day To You?", "Write Day With 3 Characters");
+
+console.log(PromptMsg);
+
