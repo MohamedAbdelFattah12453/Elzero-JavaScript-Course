12e14e1f3a3bb5a1a46be58eaa167902fe6e5296
Mohamed <mido2011fattah@gmail.com>
2026-06-24 20:11:42 +0300

    Lessons 102-110: Video 106 - BOM [Window Location Object]

---
+++ Lessons _ 102 _ 110/Lesson _ 106/main.js
@@ -0,0 +1,27 @@
+/*
+    BOM [Browser Object Model]
+    - Location Object
+    --- herf Get / Set [URL || Hash || File || Mail]
+    --- host
+    --- hash
+    --- protocol
+    --- reload()
+    --- replace()
+    --- assign()
+*/
+
+console.log(location);
+console.log(location.href);
+
+// location.href = "https://Google.com";
+// location.href = "/#sec02";
+// location.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions";
+
+// console.log(location.host);
+// console.log(location.hostname);
+
+// console.log(location.protocol);
+
+// console.log(location.hash);
+
+location.replace()
