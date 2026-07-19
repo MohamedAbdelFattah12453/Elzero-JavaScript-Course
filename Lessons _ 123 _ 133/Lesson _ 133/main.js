74db9af784e95297a51874450478ef537b187403
Mohamed <mido2011fattah@gmail.com>
2026-07-19 16:39:46 +0300

    Lesson 133: Solve The Challenge Map And Set

---
+++ Lessons _ 123 _ 133/Lesson _ 133/main.js
@@ -0,0 +1,19 @@
+/*
+    Map And Set + What You Learn => Challenge
+    Requirements
+    - You Cant Use Numbers Or True Or False
+    - Don't Use Array Indexes
+    - You Cant Use Any Higher Order Function
+    - Only One Line Solution Inside Console
+    - If You Use Length => Then Only Time Only
+    Hints
+    - You Can Use * Operator Only In Calculation
+    - Set
+    - Spread Opeartor
+    - Math Object Method
+*/
+
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log(Math.max(...new Set([...n1, ...n2])) * [...n1, ...n2].length); // 210
