88a69a150a6e0f3097405d2512e2c2421740ce5c
Mohamed <mido2011fattah@gmail.com>
2026-07-20 23:51:38 +0300

    Assignment 2 : Solve Assignment 2 From Map And Set

---
+++ Assignments _ 123 _ 133/Assignment _ 2/main.js
@@ -0,0 +1,8 @@
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+
+let newFriends = [...new Set(myFriends)].sort();
+
+console.log(newFriends);
+
+// Needed Output
+(4)[("Ahmed", "Mahmoud", "Osama", "Sayed")];
