a7ca58c6c9abdfb77ea3e42570d5e919e92de781
Mohamed <mido2011fattah@gmail.com>
2026-07-26 11:40:06 +0300

    Lesson 134: Intro And What Is Regular Expression

---
+++ Lessons _ 134 _ 146/Lesson _ 134/main.js
@@ -0,0 +1,21 @@
+/*
+    Regular Experssion
+    - Email
+    - IP
+    - Phone
+    - URL
+*/
+let str1 = `10 20 100 1000 5000`;
+let str2 = `Os1 Os12 Os123 Os1234Os Os123120s123`;
+
+let invaildEmail = `Oasma@@@gmail....com`;
+let vaildEmail = `O@nn.sa`;
+
+let ip = `192.168.2.1`; // Ipv4
+
+let url = 'elzero.org';
+let url = 'elzero.org/';
+let url = 'http://elzero.org/';
+let url = 'http://www.elzero.org/';
+let url = 'https://.elzero.org/';
+let url = 'http://www.elzero.org/?facebookid=fovkeoitjour';
