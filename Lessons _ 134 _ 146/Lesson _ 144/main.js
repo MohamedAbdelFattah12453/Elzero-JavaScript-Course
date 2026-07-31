d90e057026c585bb112a42855175d3eb5020b667
Mohamed <mido2011fattah@gmail.com>
2026-07-31 22:29:23 +0300

    Lesson 144: Regular Expression From Validation

---
+++ Lessons _ 134 _ 146/Lesson _ 144/main.js
@@ -0,0 +1,17 @@
+/*
+    Regular Expression
+    - Input From Validation Practise
+*/
+
+document.getElementById("register").onsubmit = function () {
+    let phoneInput = document.getElementById("phone").value;
+    let phoneRe = /\(\d{4}\)\s\d{3}-\d{4}/; // (1234) 567 - 8910
+    let validationResult = phoneRe.test(phoneInput);
+    console.log(validationResult);
+
+    if (validationResult === false) {
+        return false;
+}
+
+    return true;
+}
