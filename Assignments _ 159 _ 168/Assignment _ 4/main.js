e1700bd7629f03b01d1ad7244bda8da9d90f7d56
Mohamed <mido2011fattah@gmail.com>
2026-08-15 07:01:52 +0300

    Assignment 4 : Solve Assignment 4 From Age, Date, Module

---
+++ Assignments _ 159 _ 168/Assignment _ 4/main.js
@@ -0,0 +1,16 @@
+// First Solution
+let Date1 = new Date("10/25/1982");
+console.log(Date1);
+
+// Second Solution
+let Date2 = new Date("Oct 25 1982");
+console.log(Date2);
+
+// Third Solution
+let Date3 = new Date(404344800000);
+console.log(Date3);
+
+// Needed Output
+"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)";
+"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)";
+"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)";
