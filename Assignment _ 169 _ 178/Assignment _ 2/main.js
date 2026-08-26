b423597cfbb94329ea7a09295ce6735595ce42f3
Mohamed <mido2011fattah@gmail.com>
2026-08-27 02:37:18 +0300

    Solve Elzero JS Assignment 02 - AJAX & JSON Response

---
+++ Assignment _ 169 _ 178/Assignment _ 2/main.js
@@ -0,0 +1,12 @@
+let req = new XMLHttpRequest();
+
+req.open("GET", "articles.json");
+
+req.send();
+
+req.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        console.log(this.responseText);
+        console.log("Data Loaded");
+    }
+}
